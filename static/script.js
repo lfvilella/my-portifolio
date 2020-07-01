d1079d8d6db5a9740983480014ed24696ed0d509
Felipe <vilella.luisfelipe@gmail.com>
2020-07-01 20:26:08 -0300

    cool scrolling nav bar

--- static/script.js
+++ static/script.js
@@ -1,3 +1,25 @@
+// COOL SCROLLING
+
+jQuery(document).ready(function () {
+  $(document).ready(function () {
+    $('a[href^="#"].my-nav-link').on('click', function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top - 0
+      }, 900, 'swing', function () {
+        window.location.hash = target - 100;
+      });
+    });
+  });
+});
+
+
+// NAV BAR
+
 var navButton = document.querySelector('.navigation-button');
 var navMenu = document.querySelector('.navigation-menu');
 var win = window;
@@ -20,3 +42,7 @@ function closeMenu(event) {
 navButton.addEventListener('click', openMenu, false);
 
 win.addEventListener('click', closeMenu, false);
+
+
+
+// new WOW().init();
